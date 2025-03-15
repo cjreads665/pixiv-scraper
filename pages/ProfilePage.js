a9ae2f8d909c8575d8bbe83cc12bab9813685f67
Shahid Alam <shahid.alam57@gmail.com>
2025-03-15 14:59:22 +0530

    commiting for clean state

--- pages/ProfilePage.js
+++ pages/ProfilePage.js
@@ -32,13 +32,38 @@ export default class ProfilePage extends BasePage{
         // await this.page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 15000 })
     }
 
+    async clickShowAll(){
+        try {
+            // await this.page.set
+            // await delay(3000)
+            const showAllBtn = await this.page.waitForSelector(`::-p-xpath(//div[contains(text(), 'Reading works')])`, { timeout: 5000 });
+            if (showAllBtn) {
+                await showAllBtn.click();
+            }
+        } catch (e) {
+            console.log("Show all button not found");
+        }
+    }
+
     async getAllBookmarksInPage(){
         await this.clickBookmarks();
         await this.page.waitForSelector('li')
         await delay(3000) //added delay for other images to load
         const listItems = await this.page.$$('li');
         if (listItems.length > 0) {
-            await listItems[1].click();
+            await listItems[3].click();
+            /**
+             * show all is not present in some multi-image arts
+             * instead of that, we show 'reading works' -> which opens the image view of pixiv.
+             * so we need to come up with solution to capture the pixiv preview images
+             * 
+             * console:
+             * document.querySelectorAll('div[dir="vert"]')[0].children[0]
+             * this is selecting the previews alright
+             */
+            this.clickShowAll()
+
+            await delay(5000)
         } else {
             throw new Error("No list items found");
         }
